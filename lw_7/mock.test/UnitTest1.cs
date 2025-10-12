52c54f237c9dfbb1f7985fd67400df901613b1ed
Dmitrii Liashko <dmitrii.liashko>
2025-10-12 03:41:08 +0300

    Добавил элементарный консольный проект с фейк-апи (моком)

---
+++ lw_7/mock.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace mock.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ lw_7/mock/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace mock;
+
+class Program
+{
+    static async Task Main()
+    {
+        String from = "USD";
+        String to = "RUB";
+        String apiUrl = $"http://localhost:4545/rate?from={from}&to={to}";
+
+        using HttpClient httpClient = new HttpClient();
+        HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+        string json = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine($"Курс {from} -> {to}: {json}");
+    }
+}
