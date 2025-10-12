c1efd0f9ce81cf9c3ef60e5fd590fc26ef2d6576
Dmitrii Liashko <dmitrii.liashko>
2025-10-12 22:57:22 +0300

    Добавил парсеры для чисел

---
+++ lw_8/ShopProducts/Converters/ParseStringToDecimalConverter.cs
@@ -0,0 +1,39 @@
+namespace ShopProducts;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class ParseStringToDecimalConverter : JsonConverter<decimal?>
+{
+    public override decimal? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String && decimal.TryParse(reader.GetString(), out decimal result))
+        {
+            return result;
+        }
+
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            return reader.GetDecimal();
+        }
+
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return null;
+        }
+
+        throw new JsonException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, decimal? value, JsonSerializerOptions options)
+    {
+        if (value.HasValue)
+        {
+            writer.WriteNumberValue(value.Value);
+        }
+        else
+        {
+            writer.WriteNullValue();
+        }
+    }
+}
