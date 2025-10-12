5aab31e1e6fce4bfbe5e22664283b33a8970889b
Dmitrii Liashko <dmitrii.liashko>
2025-10-12 22:58:02 +0300

    Поправил сущность, добавил ранее написанные парсеры, добавил enum-классы

--- lw_8/ShopProducts/Product.cs
+++ lw_8/ShopProducts/Product.cs
@@ -1,17 +1,41 @@
 namespace ShopProducts;
 
 using System.Text.Json.Serialization;
+
 public class Product
 {
+    [JsonConverter(typeof(ParseStringToIntConverter))]
     public int? id { get; set; }
+    [JsonConverter(typeof(ParseStringToIntConverter))]
     public int? category_id { get; set; } // От 1 до 15
     public string? title { get; set; }
-    public string? alias { get; set; } // Формируется из поля title через транслит на латиницу. Но если такой алиас существует, то добавляется префикс -0
+    // Формируется из поля title через транслит на латиницу. Но если такой алиас существует, то добавляется префикс -0
+    public string? alias { get; set; } 
     public string? content { get; set; }
-    public decimal price { get; set; }
+    
+    [JsonConverter(typeof(ParseStringToDecimalConverter))]
+    public decimal? price { get; set; }
+    
+    [JsonConverter(typeof(ParseStringToDecimalConverter))]
     public decimal? old_price { get; set; }
-    public int? status { get; set; } // 0 или 1
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public ProductStatus? status { get; set; }
+    
     public string? keywords { get; set; }
     public string? description { get; set; }
-    public int? hit { get; set; } // 0 или 1
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public HitStatus? hit { get; set; }
+}
+
+public enum ProductStatus
+{
+    Inactive = 0,
+    Active = 1
+}
+public enum HitStatus
+{
+    NotHit = 0,
+    Hit = 1
 }
