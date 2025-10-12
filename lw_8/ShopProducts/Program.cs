86f51e5a74097f59daf386dc78abdebd5fcf6a59
Dmitrii Liashko <dmitrii.liashko>
2025-10-12 22:59:30 +0300

    Добавил тест запроса на получение списка товаров

--- lw_8/ShopProducts/Program.cs
+++ lw_8/ShopProducts/Program.cs
@@ -1,4 +1,23 @@
 ﻿using ShopProducts;
 
-List<Product>? products = ProductModel.LoadProducts("products.json");
-Console.WriteLine($"Загружено товаров: {products?.Count}");
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        ProductModel pm = new ProductModel();
+        List<Product>? products = await pm.LoadProducts();
+
+        if (products != null)
+        {
+            Console.WriteLine($"Получено товаров: {products.Count}");
+            foreach (Product p in products)
+            {
+                Console.WriteLine($"{p.id}: {p.title} — {p.price}₽");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Не удалось получить список товаров или он пуст.");
+        }
+    }
+}
