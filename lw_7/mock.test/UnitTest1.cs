d6103204166e6e2adca171e136fd2bbfaddd6ef6
Dmitrii Liashko <dmitrii.liashko>
2025-10-12 04:26:08 +0300

    Добавил возвращение 404, если параметры некорректны. Добавил негативный тест соответственно.

--- lw_7/mock.test/UnitTest1.cs
+++ lw_7/mock.test/UnitTest1.cs
@@ -3,7 +3,8 @@ using System.Text.Json;
 namespace mock.test;
 
 public class UnitTest1
-{
+{   
+    // Позитивный тест на получение курса USD к RUB
     [Fact]
     public async Task TestGetRate_USD_RUB()
     {
@@ -21,5 +22,22 @@ public class UnitTest1
    
         Assert.NotNull(rateResp);
         Assert.Equal(93, rateResp.Rate);
+    }
+    
+    // Негативный тест на ошибку при несуществующей валюте
+    [Fact]
+    public async Task GetRate_WithWrongCurrency_Throws()
+    {
+        string from = "USD";
+        string to = "XXX";
+        string apiUrl = $"http://localhost:4545/rate?from={from}&to={to}";
+
+        using HttpClient httpClient = new HttpClient();
+
+        await Assert.ThrowsAsync<HttpRequestException>(async () =>
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+        });
     }
 }
